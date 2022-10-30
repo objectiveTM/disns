70015b8d7dd14e775eb0ca2436e2bd43cdd5408b
5-23 <86705803+5-23@users.noreply.github.com>
2022-10-30 23:25:00 +0900

    sass setting

--- apis/emoji.js
+++ apis/emoji.js
@@ -1,5 +1,6 @@
 url = "http://127.0.0.1:5500"
 let html = document.body.innerHTML
     .replace(/{emoji:(.*)}/gim , `<img src="${url}/apis/imgs/$1.png" width=30px/>`)
+    .replace(/{emoji_url:(.*)}/gim , `${url}/apis/imgs/$1.png`)
 
 document.body.innerHTML = html;
